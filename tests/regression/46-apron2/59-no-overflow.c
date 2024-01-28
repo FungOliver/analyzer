64fc2b77141eb5fc599f674d05eaac737b830e55
oliver <ge96xey@mytum.de>
2024-01-28 17:03:00 +0100

    add new arithmic on bot exception

---
+++ tests/regression/46-apron2/59-no-overflow.c
@@ -0,0 +1,17 @@
+// PARAM: --enable ana.int.interval --set ana.activated[+] apron
+int main()
+{
+    unsigned int len = rand();
+    len = len % 10;
+    for (int i = 0; 1 - (i - 1 < len) < 1; i++) // raises both both branch dead if no_overflow is wrong computed
+    {
+        int tmp = i;
+    }
+
+    for (int i = 0; 1 - (i - 1 < len) < 1; i++) // raises both both branch dead if no_overflow is wrong computed
+    {
+        int tmp = i;
+    }
+
+    return 0;
+}
